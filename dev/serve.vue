936bb0ec3180746cd571f4414f38b2cf9c8841b3
Shlomi Goland <golandsh@gmail.com>
2021-03-31 14:40:06 +0300

    Added project

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ShlomiComponentLibrarySample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  ShlomiComponentLibrarySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <shlomi-component-library-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as ShlomiComponentLibrarySample } from './shlomi-component-library-sample.vue';
